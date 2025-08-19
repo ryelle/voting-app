c2d35150fd5b01b301d6d2c10b91906e18699818
Kelly Dwan <ryelle@users.noreply.github.com>
2025-08-19 19:23:33 -0400

    Tests: Fix Home request test, add simple Results request test

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Home", type: :request do
   describe "GET /index" do
-    it "renders a successfully" do
+    it "renders successfully" do
       get root_path
       expect(response).to be_successful
-      expect(response.body).to include('Hello, world!')
+      expect(response.body).to include('Vote.Website')
+      expect(response.body).to include('data-react-class="Home"')
     end
   end
 end
--- spec/requests/results_spec.rb
+++ spec/requests/results_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Results", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /results" do
+    it "renders successfully" do
+      get "/results"
+      expect(response).to be_successful
+      expect(response.body).to include('Vote.Website')
+      expect(response.body).to include('data-react-class="Results"')
+    end
   end
 end
