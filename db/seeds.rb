5cce6e68ace2de368c17dc1becc60e9f0c1837c8
Kelly Dwan <ryelle@users.noreply.github.com>
2025-08-19 15:20:51 -0400

    Add some candidates to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Candidate.create(name: "Person A")
+Candidate.create(name: "Person B")
